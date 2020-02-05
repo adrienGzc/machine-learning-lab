8cdebd9d78b1dfbeda6fe7269d6abae9eba474c7
Adrien Guezennec <adrien.guezennec@epitech.eu>
2020-02-05 17:49:27 +0100

    :construction: WIP: add spliting fold + start main loop to collect prediction

--- CrossValidator.py
+++ CrossValidator.py
@@ -1,9 +1,14 @@
+import random
+import pprint
+from functools import reduce
+
 class CrossValidator:
   def __init__(self, algo=None, dataset=None, nbFolds=10):
+    random.seed(1)
     self.folds = list()
 
     self.algorithm = algo
-    self.dataset = dataset
+    self.dataset = list(dataset)
     self.nbFolds = nbFolds
 
   def __checkNotEmptyAttributes(self):
@@ -12,12 +17,29 @@ class CrossValidator:
       return False
     return True
 
+  def __getFoldSize(self, nbInstances, nbFolds):
+    return round(nbInstances / nbFolds)
+
   def __splitDatasetIntoKFolds(self):
-    foldSize = round(len(self.dataset) / self.nbFolds)
-    
-    for index in range(self.nbFolds):
-      pass
+    copyDataset = self.dataset.copy()
+    random.shuffle(copyDataset)
+    foldSize = self.__getFoldSize(len(self.dataset), self.nbFolds)
+
+    for nb in range(self.nbFolds):
+      start = foldSize * nb
+      end = foldSize + start
+      self.folds.append(copyDataset[start:end])
 
   def score(self):
     if (self.__checkNotEmptyAttributes() is False):
       return False
+
+    self.__splitDatasetIntoKFolds()
+    # scores = list()
+    for index, fold in enumerate(self.folds):
+      trainData = self.folds.copy()
+      testData = fold
+      trainData.pop(index)
+      trainData = sum(trainData, [])
+
+      pprint.pprint(trainData)
--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+import random
 import pprint
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
 import NaiveBayes
+import CrossValidator
+
+def concateTargetWithDataset(dataset, targetDataset):
+  data = list()
+  for index, instance in enumerate(dataset):
+    tmp = list(instance)
+    tmp.append(targetDataset[index])
+    data.append(tmp)
+  return data
 
 def main():
   irisData = datasets.load_iris()
   irisData.data = irisData.data[:-50]
   irisData.target = irisData.target[:-50]
 
+  newDataset = concateTargetWithDataset(irisData.data, irisData.target)
   X_train, X_test, y_train, y_test = train_test_split(irisData.data, irisData.target, test_size=0.2, random_state=0)
 
   naiveBayes = NaiveBayes.NaiveBayes()
+  crossValidator = CrossValidator.CrossValidator(algo=naiveBayes, dataset=newDataset, nbFolds=5)
+  crossValidator.score()
   predict = naiveBayes.fit(trainData=X_train, trainTarget=y_train, testData=X_test, testTarget=y_test).predict()
-  print(predict)
+  # print(predict)
 
 if __name__ == "__main__":
     main()
